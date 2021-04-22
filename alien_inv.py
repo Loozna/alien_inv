0565ab9a70d262702f8161412776efe6b8b9d769
Michał <michal.marciniak90@wp.pl>
2021-04-23 00:37:06 +0200

    Wyświetlanie okna, klasa ustawien

--- alien_inv.py
+++ alien_inv.py
@@ -1,1 +1,19 @@
-
+import sys
+import pygame
+from settings import Settings
+def run_game():
+	ai_settings = Settings()
+	pygame.init()
+	screen = pygame.display.set_mode((
+	ai_settings.screen_width,
+	ai_settings.screen_height))
+	pygame.display.set_caption('Inwazja obcych')
+	bg_color = ai_settings.bg_color
+	while True:
+		screen.fill(bg_color)
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
+		pygame.display.flip()
+				
+run_game()
